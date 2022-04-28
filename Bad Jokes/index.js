13410ef4864fa53cee6f23348e06fe9858291181
rohanniraula2021140 <81124145+rohanniraula2021140@users.noreply.github.com>
2022-04-28 12:30:09 +0545

    Js Project with exciting UI
    Project with Exciting UI

--- Bad Jokes/index.js
+++ Bad Jokes/index.js
@@ -6,30 +6,59 @@ const jokes1 = document.querySelector('.java');
 const jokes2 = document.querySelector('.python');
 const jokes3 = document.querySelector('.cSharp');
 
-const generateJokes = (num) =>{
+const generateJokes = async (num) =>{
     const setHeader = {
         headers:{
             Accept:"application/json"
         }
     }
-    fetch('https://icanhazdadjoke.com' , setHeader)
-    .then((res)=>res.json())
-    .then((data)=>{
-        if(num == 1){
+    try{
+    const res = await fetch('https://icanhazdadjoke.com' , setHeader);
+    
+    const data = await res.json();
+    if(num == 1){
 
-            jokes1.innerHTML = data.joke; 
-        }if(num == 2){
+        jokes1.innerHTML = data.joke; 
+    }if(num == 2){
 
-            jokes2.innerHTML = data.joke; 
-        }if(num== 3){
+        jokes2.innerHTML = data.joke; 
+    }if(num== 3){
 
-            jokes3.innerHTML = data.joke; 
-        }
-    })
-    .catch((error)=>{
-        console.log(error);
-    }) 
+        jokes3.innerHTML = data.joke; 
+    }
+}catch(error){
+    console.log(`The error is ${error}`);
 }
+    
+}
+
+
+    
+
+// const generateJokes = (num) =>{
+//     const setHeader = {
+//         headers:{
+//             Accept:"application/json"
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com' , setHeader)
+//     .then((res)=>res.json())
+//     .then((data)=>{
+//         if(num == 1){
+
+//             jokes1.innerHTML = data.joke; 
+//         }if(num == 2){
+
+//             jokes2.innerHTML = data.joke; 
+//         }if(num== 3){
+
+//             jokes3.innerHTML = data.joke; 
+//         }
+//     })
+//     .catch((error)=>{
+//         console.log(error);
+//     }) 
+// }
 
 
 
